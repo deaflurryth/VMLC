f4148402d24db8c188affa7cf016dd2560503bb4
deaflurryth <skullangel78@mail.ru>
2023-12-03 05:29:51 +0300

    loginRegisterResults

--- server/settings.py
+++ server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-zaye9g3p9=pdjth9(t$+k!7^fz9ihby@%v%1-iih_y&t)__xfq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
